8dc153bf5991de8056d8915132abc0c2ad3f06f6
Cheyenne Gowie <yenniegowie6@gmail.com>
2026-03-05 23:13:49 -0500

    Added javaScript for website

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,16 @@
 /* Add your Application JavaScript */
+
+window.addEventListener("load", function() {
+    const flashes = document.querySelectorAll('.flashes li');
+    flashes.forEach(function(flash) {
+        setTimeout(function() {
+            flash.style.transition = "opacity 0.5s ease, transform 0.5s ease";
+            flash.style.opacity = "0";
+            flash.style.transform = "translateY(-10px)"; 
+            setTimeout(function() {
+                flash.remove();
+            }, 500); 
+        }, 4000); 
+    });
+});
+
